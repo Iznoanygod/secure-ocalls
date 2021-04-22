e316689db066356a2f4f80f0d18f6e9c54cdf95d
Andrew Park <mkbandrewp@gmail.com>
2021-04-22 04:04:40 -0400

    modification detection basics

--- LibraryModificationDetection/library.c
+++ LibraryModificationDetection/library.c
@@ -3,10 +3,8 @@
 #include <string.h>
 #include <openssl/sha.h>
 
-__attribute__((constructor))
-void constructor(){
-    printf("library has been loaded\n");
-}
+char test_1_digest[20];
+char test_2_digest[20];
 
 int test_1() {
     return 1 ? 1 : 0;
@@ -16,5 +14,20 @@ void test_2(){
     char str[] = "this function has not been modified\n";
     char poisoned_string[] = "this function has been modified\n";
     printf(str);
+}
+
+int check_modification(){
+    char test_1_hash[20];
+    char test_2_hash[20];
+    SHA1((char*)test_1, 0xb, test_1_hash);
+    SHA1((char*)test_2, 0xaa, test_2_hash);
+    return memcmp(test_1_hash, test_1_digest, 20) & memcmp(test_2_hash, test_2_digest, 20);
+}
+
+__attribute__((constructor))
+void constructor(){
+    printf("library has been loaded\n");
+    SHA1((char*)test_1, 0xb, test_1_digest);
+    SHA1((char*)test_2, 0xb, test_2_digest);
 }
 
--- LibraryModificationDetection/main.c
+++ LibraryModificationDetection/main.c
@@ -5,6 +5,10 @@ char poisoned_string[] = "the function has been modified\n";
 int main(int argc, char** argv){
     printf("loading the library\n");
     void* handle = dlopen("./library.so", RTLD_NOW);
+    int (*check_modification)() = dlsym(handle, "check_modification");
+    printf("checking if library was modified\n");
+    int isModified = check_modification();
+    printf(isModified ? "library was modified\n" : "library was not modified\n");
     int (*fcnptr_1)() = dlsym(handle, "test_1");
     printf("test function value: %d\n", fcnptr_1());
     printf("changing memory page protections\n");
@@ -13,15 +17,17 @@ int main(int argc, char** argv){
     printf("trying to change function return value\n");
     *((char*)fcnptr_1 + 5) = 0;
     printf("test function value: %d\n", fcnptr_1());
-    
     void (*fcnptr_2)() = dlsym(handle, "test_2");
     printf("testing library print function\n");
     fcnptr_2();
     printf("modifying library string function\n");
     mprotect((void*)((unsigned long) fcnptr_2 - ((unsigned long) fcnptr_2 % getpagesize())),
         getpagesize(), PROT_READ | PROT_WRITE | PROT_EXEC);
-    //unsigned long long patch = 0x2e7b;
-    //memcpy(fcnptr_2+7, &patch, 2);
+    unsigned long long patch = 0xa0;
+    memcpy(fcnptr_2+133, &patch, 1);
     fcnptr_2();
+    printf("checking if library was modified\n");
+    isModified = check_modification();
+    printf(isModified ? "library was modified\n" : "library was not modified\n");
     dlclose(handle);
 }
